94834d1774a99cd8b63ae99edb98cc204ccaea8c
Alex <sanyaigas88@gmail.com>
2023-10-09 15:05:40 +0300

    3-1_v2

--- src/main.js
+++ src/main.js
@@ -1,4 +1,13 @@
-import { createApp } from 'vue'
-import App from './App.vue'
+// src/main.js
+import { createApp } from 'vue';
+import App from './App.vue';
+import io from 'socket.io-client';
 
-createApp(App).mount('#app')
+const socket = io('http://localhost:3000');
+console.log('Connected to server');
+
+socket.on('message', message => {
+  console.log('Received message:', message);
+});
+
+createApp(App).mount('#app');
