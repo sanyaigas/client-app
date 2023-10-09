d6f57c15a9b4766e01bbf01ebc3cfce7a3e493ba
Alex <sanyaigas88@gmail.com>
2023-10-09 15:21:13 +0300

    3-2_v2

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,59 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="app">
+    <div class="chat">
+      <div class="messages">
+        <div v-for="message in messages" :key="message.id">{{ message.text }}</div>
+      </div>
+      <input v-model="newMessage" @keyup.enter="sendMessage" placeholder="Type a message" />
+    </div>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import io from 'socket.io-client';
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+  data() {
+    return {
+      newMessage: '',
+      messages: [],
+      socket: null,
+    };
+  },
+  created() {
+    this.socket = io('http://localhost:3000');
+    this.socket.on('message', this.receiveMessage);
+    this.socket.on('connect', () => console.log('Connected to server'));
+    this.socket.on('disconnect', () => console.log('Disconnected from server'));
+  },
+  methods: {
+    sendMessage() {
+      if (this.newMessage.trim() !== '') {
+        this.socket.emit('message', this.newMessage);
+        this.newMessage = '';
+      }
+    },
+    receiveMessage(message) {
+      this.messages.push({ text: message, id: this.messages.length });
+    },
+  },
+};
 </script>
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
   margin-top: 60px;
+}
+.chat {
+  width: 300px;
+  margin: auto;
+}
+.messages {
+  border: 1px solid #ccc;
+  padding: 10px;
+  max-height: 200px;
+  overflow-y: auto;
+  margin-bottom: 10px;
 }
 </style>
